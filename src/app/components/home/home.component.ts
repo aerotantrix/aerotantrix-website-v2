14da3b988283480f71acefa210dd1cefd67748a6
Sharath Chandra Nirmala <64958512+sharathnirmala16@users.noreply.github.com>
2023-11-27 16:42:39 +0530

    changes to home background image display mechanism

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  crop = false;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (window.innerWidth < 700) {
+      this.crop = true;
+    } else {
+      this.crop = false;
+    }
+  }
 }
